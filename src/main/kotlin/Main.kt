047c6340195ad27f8afca5c8bfdcfd4fef2d715d
Cuong <cuong.nh@cep.org.vn>
2023-02-27 16:25:32 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+//    println("Hello World!")
+//
+//    // Try adding program arguments via Run/Debug configuration.
+//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
+//    1: Khai báo biến
+//    mutable,immutable
+//
+//    var name ="Ngo Huy Cuong"
+//    name = "Ngo Huy Cuong 1"
+//    print (name)
+
+        val pi = 3.14
+        val address = "199 Ly thuong kiet"
+
+
+
+}
